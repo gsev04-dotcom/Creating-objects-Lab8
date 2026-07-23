895089d23fa54c6ea25d7cc3906b40b6582817d9
Gretle Severin-Augustin <gsev04@gmail.com>
2026-07-23 13:29:08 -0400

    Rename person object to student

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 
-let person =  {
+let student =  {
 		name :"Jacqueline Prescott",
 		age: 45,
 		enrolled : true,
 		courses:["Science", "Maths", "English", "Biology"],
      
 		displayInfo: function() {
-				return "Name: " + this.name + " Age: " + this.age  + " Enrolled: " + this.enrolled;
-			}
+		return "Student: " + this.name + " Age: " + this.age  + " Enrolled: " + this.enrolled;
+}
 };
 
-console.log("Name:", person.name);
-console.log ("Age:", person.age);
-console.log(person.displayInfo());
+console.log("Name:", student.name);
+console.log ("Age:", student.age);
+console.log(student.displayInfo());
+
+
+
+
