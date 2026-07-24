705cdd377d0085e73006de49343910493408db5e
Gretle Severin-Augustin <gsev04@gmail.com>
2026-07-23 20:53:42 -0400

    Clone student object using spread operator and added  a new property

--- script.js
+++ script.js
@@ -35,3 +35,9 @@ console.log (name, courses);
 let scores =[85,92,78,90];
 let [firstScore ,secondScore] =scores;
 console.log(firstScore,secondScore);
+
+
+
+
+let clonedStudent = {student, graduationYear: 2029};
+console.log(clonedStudent);
